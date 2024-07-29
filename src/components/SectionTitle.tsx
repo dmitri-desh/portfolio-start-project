0fdb273d5a04e543c32d618ac7ddddb0096e47e6
Dmitry Deshko <dmitri.desh@gmail.com>
2024-07-29 19:28:12 +0300

    refactor: Skills part 3. Responsibility add

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,14 +1,18 @@
 import styled from "styled-components";
 import {theme} from "../styles/Theme";
+import {font} from "../styles/Common";
 
 export const SectionTitle = styled.h2`
-    font-family: 'Josefin Sans', sans-serif;
-    font-size: 36px;
-    font-weight: 600;
+    ${font({
+        family: "'Josefin Sans', sans-serif",
+        weight: 600,
+        Fmin: 30,
+        Fmax: 36
+    })};
     letter-spacing: 5px;
     text-align: center;
     margin-bottom: 90px;
-    
+
     position: relative;
 
     &::before {
@@ -17,10 +21,14 @@ export const SectionTitle = styled.h2`
         width: 55px;
         height: 1px;
         border: 1px solid ${theme.colors.accent};
-        
+
         position: absolute;
         left: 50%;
         bottom: -30px;
         transform: translateX(-50%);
+        
+        @media ${theme.media.mobile} {
+            bottom: -24px;
+        }
     }
 `;
