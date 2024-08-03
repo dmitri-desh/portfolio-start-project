ca9d3b301c3e69e5a820465f41f6efe788bf15cd
Dmitry Deshko <dmitri.desh@gmail.com>
2024-08-03 23:19:36 +0300

    refactor: Testimony section

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
+import React, {FC} from 'react';
 import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle";
 import {Icon} from "../../../components/icon/Icon";
 import {Slider} from "../../../components/slider/Slider";
 import {FlexWrapper} from "../../../components/FlexWrapper";
-import {IconWrapper} from "../skills/skill/Skill";
 import {Container} from "../../../components/Container";
+import {S} from "../skills/Skills_Styles";
 
-export const Testimony = () => {
+export const Testimony: FC = () => {
     return (
         <StyledTestimony>
             <Container>
                 <SectionTitle>Testimony</SectionTitle>
                 <FlexWrapper direction="column" alignItems="center">
-                    <IconWrapper>
+                    <S.IconWrapper>
                         <Icon iconId="quoteAltRight"/>
-                    </IconWrapper>
+                    </S.IconWrapper>
                     <Slider/>
                 </FlexWrapper>
             </Container>
@@ -27,7 +27,7 @@ const StyledTestimony = styled.section`
     background-color: #81ed61;
     min-height: 50vh;
     
-    ${IconWrapper} {
+    ${S.IconWrapper} {
         margin: 28px 0 72px;
     }
 `;
