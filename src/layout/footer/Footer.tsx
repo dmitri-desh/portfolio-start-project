a97c44555ba0cd73b03853c4cbe4d02302ac30dd
Dmitry Deshko <dmitri.desh@gmail.com>
2024-08-03 23:22:31 +0300

    refactor: Footer section

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,89 +1,41 @@
-import React from 'react';
-import styled from "styled-components";
+import React, {FC} from 'react';
 import {Icon} from "../../components/icon/Icon";
 import {FlexWrapper} from "../../components/FlexWrapper";
-import {theme} from "../../styles/Theme";
-import {font} from "../../styles/Common";
+import {S} from "./Footer_Styles";
 
-export const Footer = () => {
+const socialsItemsData = [
+    {
+        iconId: "instagram"
+    },
+    {
+        iconId: "telegram"
+    },
+    {
+        iconId: "vk"
+    },
+    {
+        iconId: "linkedin"
+    }
+];
+
+export const Footer: FC = () => {
     return (
-        <StyledFooter>
+        <S.Footer>
             <FlexWrapper direction="column" alignItems="center">
-                <Name>Svetlana</Name>
-                <SocialsList>
-                    <SocialItem>
-                        <SocialLink>
-                            <Icon iconId="instagram" height="21px" width="21px" viewBox="0 0 21px 21px"/>
-                        </SocialLink>
-                    </SocialItem>
-                    <SocialItem>
-                        <SocialLink>
-                            <Icon iconId="telegram" height="21px" width="21px" viewBox="0 0 21px 21px"/>
-                        </SocialLink>
-                    </SocialItem>
-                    <SocialItem>
-                        <SocialLink>
-                            <Icon iconId="vk" height="21px" width="21px" viewBox="0 0 21px 21px"/>
-                        </SocialLink>
-                    </SocialItem>
-                    <SocialItem>
-                        <SocialLink>
-                            <Icon iconId="linkedin" height="21px" width="21px" viewBox="0 0 21px 21px"/>
-                        </SocialLink>
-                    </SocialItem>
-                </SocialsList>
-                <Copyright>© 2023 Svetlana Dyablo, All Rights Reserved.</Copyright>
+                <S.Name>Svetlana</S.Name>
+                <S.SocialsList>
+                    {socialsItemsData.map((item, index) => {
+                        return (
+                            <S.SocialItem key={index}>
+                                <S.SocialLink>
+                                    <Icon iconId={item.iconId} height="21px" width="21px" viewBox="0 0 21px 21px"/>
+                                </S.SocialLink>
+                        </S.SocialItem>
+                        )
+                    })}
+                </S.SocialsList>
+                <S.Copyright>© 2023 Svetlana Dyablo, All Rights Reserved.</S.Copyright>
             </FlexWrapper>
-        </StyledFooter>
+        </S.Footer>
     );
 };
-
-const StyledFooter = styled.footer`
-    background-color: ${theme.colors.primaryBg};
-    padding: 40px 0;
-`;
-
-const Name = styled.span`
-    ${font({
-        family: "'Josefin Sans', sans-serif",
-        weight:700,
-        Fmin: 16,
-        Fmax: 22
-    })};
-    letter-spacing: 3px;
-`;
-
-const SocialsList = styled.ul`
-    display: flex;
-    gap: 20px;
-    margin: 30px 0;
-`;
-
-const SocialItem = styled.li`
-
-`;
-
-const SocialLink = styled.a`
-    background-color: rgba(255, 255, 255, 0.1);
-    border-radius: 50%;
-    width: 35px;
-    height: 35px;
-    
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    
-    color: ${theme.colors.accent};
-    
-    &:hover {
-        color: ${theme.colors.primaryBg};
-        transform: translateY(-4px);
-    }
-`;
-
-const Copyright = styled.small`
-    font-size: 12px;
-    font-weight: 400;
-    text-align: center;
-    opacity: 0.5;
-`;
