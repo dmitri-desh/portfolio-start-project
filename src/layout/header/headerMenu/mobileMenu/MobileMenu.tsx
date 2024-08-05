90ba234fe76e69efa2849d63693b409d717928b7
Dmitry Deshko <dmitri.desh@gmail.com>
2024-08-05 11:13:36 +0300

    refactor: Burger menu "useState" hook add

--- src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
+++ src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
@@ -1,14 +1,18 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import {Menu} from "../menu/Menu";
 import {S} from "../HeaderMenu_Styles";
 
 export const MobileMenu: FC<{ menuItems: string[] }> = (props: { menuItems: string[] }) => {
+
+    const [menuIsOpen,setMenuIsOpen] = useState(false);
+    const onBurgerBtnClick = () => setMenuIsOpen(!menuIsOpen);
+
     return (
         <S.MobileMenu>
-            <S.BurgerButton isOpen={false}>
+            <S.BurgerButton onClick={onBurgerBtnClick} isOpen={menuIsOpen}>
                 <span></span>
             </S.BurgerButton>
-            <S.MobileMenuPopup isOpen={false}>
+            <S.MobileMenuPopup onClick={() => setMenuIsOpen(false)} isOpen={menuIsOpen}>
                 <Menu menuItems={props.menuItems} />
             </S.MobileMenuPopup>
         </S.MobileMenu>
