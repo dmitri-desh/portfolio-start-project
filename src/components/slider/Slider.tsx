eb77bb766bf1550c5e91b7562c58653f4fc0d03b
Dmitry Deshko <dmitri.desh@gmail.com>
2024-08-08 12:57:18 +0300

    refactor: AliceCarousel for slider add

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,21 +1,45 @@
-import React, {FC} from 'react';
-import {FlexWrapper} from "../FlexWrapper";
+import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import '../../styles/slider.css';
 import {S} from "./Slider_Styles";
+import {Icon} from "../icon/Icon";
 
-export const Slider: FC = () => {
+type SlidePropsType = {
+    text: string,
+    userName: string
+}
+
+const Slide = (props: SlidePropsType) => {
     return (
-        <S.Slider>
-            <FlexWrapper>
-                <S.Slide>
-                    <S.Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</S.Text>
-                    <S.Name>@ivan ivanow</S.Name>
-                </S.Slide>
-            </FlexWrapper>
-            <S.Pagination>
-                <span></span>
-                <span className="active"></span>
-                <span></span>
-            </S.Pagination>
-        </S.Slider>
-    );
+        <S.Slide>
+            <S.Text>{props.text}</S.Text>
+            <S.Name>@{props.userName}</S.Name>
+        </S.Slide>
+    )
 };
+
+const items = [
+    <Slide userName={"ivan ivanow"}
+           text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."}
+    />,
+    <Slide userName={"vasia pupkin"}
+           text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."}
+    />,
+    <Slide userName={"john smith"}
+           text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."}
+    />
+];
+
+export const Slider = () => (
+    <S.Slider>
+        <AliceCarousel
+            mouseTracking
+            items={items}
+            renderPrevButton={() => {
+                return <Icon iconId={"code"}/>
+            }}
+        />
+    </S.Slider>
+);
+
